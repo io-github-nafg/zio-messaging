bc0952717a237f54b56b791a2714547460f5d657
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2024-10-31 01:51:32 -0400

    Remove redundant import in TwilioClient.scala
    Eliminated unused Config import statement in TwilioClient.scala to improve code cleanliness and maintainability. No functional changes were made.

--- zio-messaging-twilio/src/scala/io/github/nafg/messaging/twilio/TwilioClient.scala
+++ zio-messaging-twilio/src/scala/io/github/nafg/messaging/twilio/TwilioClient.scala
@@ -1,13 +1,12 @@
 package io.github.nafg.messaging.twilio
 
-
 import com.twilio.http.TwilioRestClient
-import zio.{Config, ZLayer}
+import zio.ZLayer
 
 case class TwilioClient(twilioRestClient: TwilioRestClient)
 object TwilioClient {
   case class Config(accountSid: String, authToken: String)
-  
+
   val layer = ZLayer.fromFunction { (config: Config) =>
     TwilioClient(
       new TwilioRestClient.Builder(config.accountSid, config.authToken)
